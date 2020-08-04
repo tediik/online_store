4a780b224c57060e9abd86469aa905d8fc5a614b
Eduard Nabiev <tediik@gmail.com>
2020-08-04 17:43:18 +0300

    Init commit

---
+++ src/main/java/com/jm/online_store/online_store/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jm.online_store.online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jm/online_store/online_store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.online_store.online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
