6151bca3536dd28c125b2671f66322e177529566
DaveShedar <62851456+DaveShedar@users.noreply.github.com>
2020-08-19 18:43:38 +0300

    DataProductInitilizer

--- src/main/java/com/jm/online_store/OnlineStoreApplication.java
+++ src/main/java/com/jm/online_store/OnlineStoreApplication.java
@@ -1,12 +1,30 @@
 package com.jm.online_store;
+
+import com.jm.online_store.service.CategoriesService;
+import com.jm.online_store.service.ProductService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class OnlineStoreApplication {
 
+	private static ProductService productService;
+
+
+	public OnlineStoreApplication(ProductService productService){
+		this.productService = productService;
+	}
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreApplication.class, args);
+
+		System.out.println(productService.findProductById(7L));
+
+
 	}
 
 }
