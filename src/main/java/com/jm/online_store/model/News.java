0954543ead0ce08d80149c63b4479d385726cefc
Dmitry Shishkin <shishkindima@gmail.com>
2020-08-20 16:14:49 +0300

    removed unused imports

--- src/main/java/com/jm/online_store/model/News.java
+++ src/main/java/com/jm/online_store/model/News.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
