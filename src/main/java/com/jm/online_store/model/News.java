6dd63efc9bf33e679733a9db84e6f3127339ead9
siraj_dev <y-taksa@yandex.ru>
2020-08-21 12:51:32 +0500

    update

--- src/main/java/com/jm/online_store/model/News.java
+++ src/main/java/com/jm/online_store/model/News.java
@@ -1,22 +1,22 @@
 package com.jm.online_store.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class News {
 
