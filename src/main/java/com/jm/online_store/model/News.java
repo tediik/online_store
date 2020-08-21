0550b948eb90339bd52c0a59559f36dd0e4b3827
siraj_dev <y-taksa@yandex.ru>
2020-08-21 14:52:15 +0500

    update

--- src/main/java/com/jm/online_store/model/News.java
+++ src/main/java/com/jm/online_store/model/News.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
