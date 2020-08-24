cd9fb526aafbdf1fca62efdee356dbda793ba636
Dmitry Shishkin <shishkindima@gmail.com>
2020-08-24 16:42:04 +0300

    added configuration class to enable and configure Scheduling

---
+++ src/main/java/com/jm/online_store/config/scheduling/SchedulingConfiguration.java
@@ -0,0 +1,11 @@
+package com.jm.online_store.config.scheduling;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+public class SchedulingConfiguration {
+}
