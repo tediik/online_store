1c9c5667ffa2c5c231f0ebc0e573a1a2a2487bc9
siraj_dev <y-taksa@yandex.ru>
2020-08-24 12:29:42 +0500

    added javadoc

--- src/main/java/com/jm/online_store/controller/rest/ManagerRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/ManagerRestController.java
@@ -3,11 +3,13 @@ package com.jm.online_store.controller.rest;
 import com.jm.online_store.model.News;
 import com.jm.online_store.service.interf.NewsService;
 import lombok.AllArgsConstructor;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Рест контроллер для управления новостями из кабинете менеджера, а также публикации новостей
+ * на странице новостей
+ */
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "api/manager", method = RequestMethod.GET)
@@ -22,73 +28,58 @@ public class ManagerRestController {
 
     private final NewsService newsService;
 
-    @GetMapping
-    public String managerPage(){
-        return "managerPage";
+    /**
+     * Метод возвращающий всписок всех новостей
+     * @return List<News> возвращает список всех новстей из базы данных
+     */
+    @GetMapping("/news")
+    public ResponseEntity<List<News>> allNews() {
+
+        List<News> allNewsList = newsService.findAll();
+        return ResponseEntity.ok().body(allNewsList);
     }
 
-    /*@GetMapping("/news")
-    public String newsManagement(Model model) {
-
-        List<News> newsList = newsService.findAll();
-
-        List<News> publishedNewsList = newsService
-                .findAllByPostingDateBefore(LocalDateTime.now());
-
-        List<News> unpublishedNewsList = newsService
-                .findAllByPostingDateAfter(LocalDateTime.now());
-
-        model.addAttribute("news", newsList);
-        model.addAttribute("publishedNews", publishedNewsList);
-        model.addAttribute("unpublishedNews", unpublishedNewsList);
-        return "newsManagement";
-    }*/
-
-    @GetMapping("/news/add")
-    public String newsAdd(Model model) {
-
-        model.addAttribute("news", new News());
-        return "newsAdd";
-    }
-
+    /**
+     * Метод сохраняет новости в базу данных
+     * @param news сущность для сохранения в базе данных
+     * @return возвращает заполненную сущность клиенту
+     */
     @PostMapping("/news/post")
-    public String newsPost(News news) {
+    public ResponseEntity<News> newsPost(@RequestBody News news) {
 
         if(news.getPostingDate() == null || news.getPostingDate().isBefore(LocalDateTime.now())) {
             news.setPostingDate(LocalDateTime.now().withSecond(0).withNano(0));
         }
 
         newsService.save(news);
-        return "redirect:/manager/news";
+        return ResponseEntity.ok().body(news);
     }
 
-    @GetMapping("/news/{id}/edit")
-    public String newsDetails(@PathVariable(value = "id") Long id, Model model) {
-
-        if (!newsService.existsById(id)) {
-            return "redirect:/manager/news";
-        }
-
-        News news = newsService.findById(id).orElseGet(News::new);
-        model.addAttribute("news", news);
-        return "newsEdit";
-    }
-
-    @PostMapping("/news/update")
-    public String newsUpdate(News news) {
+    /**
+     * Метод обновляет сущность в базе данных
+     * @param news сущность для сохранения в базе данных
+     * @return возвращает обновленную сущность клиенту
+     */
+    @PutMapping("/news/update")
+    public ResponseEntity<News> newsUpdate(@RequestBody News news) {
 
         if(news.getPostingDate() == null || news.getPostingDate().isBefore(LocalDateTime.now())) {
             news.setPostingDate(LocalDateTime.now().withSecond(0).withNano(0));
         }
 
         newsService.save(news);
-        return "redirect:/manager/news";
+        return ResponseEntity.ok().body(news);
     }
 
+    /**
+     * Метод удаляет сушность из базы данных по уникальному идентификатору
+     * @param id уникальный идентификатор
+     * @return возвращает идентификатор удаленной сущности клиенту
+     */
     @DeleteMapping("/news/{id}/delete")
-    public String newsDelete(@PathVariable Long id) {
+    public ResponseEntity<Long> newsDelete(@PathVariable Long id) {
 
         newsService.deleteById(id);
-        return "redirect:/manager/news";
+        return ResponseEntity.ok().body(id);
     }
 }
