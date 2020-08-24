2879daf8d403394d0a8aabecb94e0c7d834a99c7
Dmitry Shishkin <shishkindima@gmail.com>
2020-08-24 23:00:01 +0300

    added js scripts for checkbox

--- src/main/resources/static/js/customerProfilePageFunctions.js
+++ src/main/resources/static/js/customerProfilePageFunctions.js
@@ -1,12 +1,20 @@
 const stockMailingCheckbox = document.getElementById('#stockMailingCheckbox')
-
+/**
+ * Обработка чекбокса #stockMailingCheckbox
+ * если галка стоит, то отображать dropdownlist
+ * с выбором дня недели для рассылки акций
+ * если галку убрать, то скрывать dropdownlist
+ */
 $('#stockMailingCheckbox').change(function(){
     if(this.checked!==true){
-        $(".day-of-the-week").addClass("d-none")
-        console.log("hide")
+        $(".day-of-the-week-drop-list").addClass("d-none")
     }
     else{
-        $(".day-of-the-week").removeClass("d-none")
-        console.log("show")
+        $(".day-of-the-week-drop-list").removeClass("d-none")
     }
 });
+
+$('#dayOfWeekDropList').change(function (){
+    let selectedValue = $(this).val()
+    console.log(selectedValue)
+})
