4bfda266ca57b9bacdb9b34b7a581cd6b4c50170
neuromanzer <krokusfs@yahoo.com>
2020-08-26 14:10:23 +0300

    43-убрать-связывание-на-полях-использовать-через-конструктор-пройтись-по-всему-проекту-вместо-конструктора-аннотация-ломбок
    убраны пустые строки

--- src/main/java/com/jm/online_store/controller/simple/CustomerController.java
+++ src/main/java/com/jm/online_store/controller/simple/CustomerController.java
@@ -10,11 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -25,11 +21,8 @@ import java.util.List;
 public class CustomerController {
 
     private final UserService userService;
-
     private final PasswordEncoder passwordEncoder;
-
     private final RoleService roleService;
-
     private final OrderService orderService;
 
     @GetMapping
--- src/main/java/com/jm/online_store/controller/simple/RegistrationController.java
+++ src/main/java/com/jm/online_store/controller/simple/RegistrationController.java
@@ -7,7 +7,6 @@ import com.jm.online_store.service.interf.UserService;
 import com.jm.online_store.util.ValidationUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,9 +24,7 @@ import javax.servlet.http.HttpServletRequest;
 public class RegistrationController {
 
     private final RoleService roleService;
-
     private final UserService userService;
-
     private final ConfirmationTokenRepository confirmationTokenRepository;
 
     //private ValidationUtils validationUtils;
@@ -44,17 +41,17 @@ public class RegistrationController {
             log.debug("BindingResult in registerUserAccount hasErrors: {}", bindingResult);
             return "registration";
         }
-        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())){
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
             log.debug("Passwords do not match : passwordConfirmError");
             model.addAttribute("passwordConfirmError", "Пароли не совпадают");
             return "registration";
         }
-        if (userService.isExist(userForm.getEmail())){
+        if (userService.isExist(userForm.getEmail())) {
             log.debug("User with same email already exists : emailError ");
             model.addAttribute("emailError", "Пользователь с таким именем уже существует");
             return "registration";
         }
-        if (ValidationUtils.isNotValidEmail(userForm.getEmail())){
+        if (ValidationUtils.isNotValidEmail(userForm.getEmail())) {
             log.debug("Wrong email! Не правильно введен email");
             model.addAttribute("emailError", "Не правильно введен email");
             return "registration";
