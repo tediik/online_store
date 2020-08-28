18f84c66ced6cacf635413c8480f79b7f9fb39b5
Anton <anton.moskvin1@gmail.com>
2020-08-28 15:32:40 +0300

    work

---
+++ src/main/java/com/jm/online_store/config/security/Twitter/TwitterAuth.java
@@ -0,0 +1,17 @@
+//package com.jm.online_store.config.security.Twitter;
+//
+//import com.jm.online_store.service.interf.RoleService;
+//import com.jm.online_store.service.interf.UserService;
+//import lombok.AllArgsConstructor;
+//import lombok.NoArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Service
+//public class TwitterAuth {
+//
+//
+//}
