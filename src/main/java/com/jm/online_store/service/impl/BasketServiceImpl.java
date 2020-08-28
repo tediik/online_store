880dff12d2d7019df5709ccd2dafc8b6a819b93e
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2020-08-28 10:26:53 +0300

    Update BasketServiceImpl.java

--- src/main/java/com/jm/online_store/service/impl/BasketServiceImpl.java
+++ src/main/java/com/jm/online_store/service/impl/BasketServiceImpl.java
@@ -6,52 +6,26 @@ import com.jm.online_store.service.interf.BasketService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-/**
- * слой сервиса - бизнеслогики для сущности SubBasket.
- */
 @Service
 @AllArgsConstructor
 public class BasketServiceImpl implements BasketService {
     private final BasketRepository basketRepository;
 
-    /**
-     * метод поиска корзины по id.
-     *
-     * @param idBasket идентификатор корзины
-     * @return корзина.
-     */
     @Override
     public SubBasket findBasketById(Long idBasket) {
         return basketRepository.findById(idBasket).get();
     }
 
-    /**
-     * метод обновления корзины.
-     *
-     * @param subBasket корзина для обновления.
-     * @return корзина.
-     */
     @Override
     public SubBasket updateBasket(SubBasket subBasket) {
         return basketRepository.saveAndFlush(subBasket);
     }
 
-    /**
-     * метод добавления корзины.
-     *
-     * @param subBasket корзина для обновления.
-     * @return корзина.
-     */
     @Override
     public SubBasket addBasket(SubBasket subBasket) {
         return basketRepository.save(subBasket);
     }
 
-    /**
-     * метод удаления корзины.
-     *
-     * @param subBasket корзина.
-     */
     @Override
     public void deleteBasket(SubBasket subBasket) {
         basketRepository.delete(subBasket);
