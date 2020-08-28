56e7364ed7879a0f23dfa7c5f455434d58f57a85
IvAndreev1618 <ivandreev1618@gmail.com>
2020-08-28 14:17:39 +0300

    Функционал перенесен в REST-контроллер. Удалена лишняя сущность GenericResponce и Контроллер регистрации. Все скрипты кроме нужных на предзагрузке страницы перенесены в соответствующий файл. У админа и менеджера убран доступ к кнопке регистрации.

--- src/main/java/com/jm/online_store/controller/simple/MainPageController.java
+++ src/main/java/com/jm/online_store/controller/simple/MainPageController.java
@@ -1,35 +1,14 @@
 package com.jm.online_store.controller.simple;
 
-import com.jm.online_store.model.GenericResponce;
 import com.jm.online_store.model.User;
-import com.jm.online_store.repository.ConfirmationTokenRepository;
-import com.jm.online_store.service.interf.RoleService;
-import com.jm.online_store.service.interf.UserService;
-import com.jm.online_store.util.ValidationUtils;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/")
-@Slf4j
 public class MainPageController {
-
-    @Autowired
-    private UserService userService;
-
-    private ValidationUtils validationUtils;
 
     @GetMapping
     public String mainPage(Model model) {
@@ -37,32 +16,4 @@ public class MainPageController {
         return "mainPage";
     }
 
-    @PostMapping("/registration")
-    @ResponseBody
-    public GenericResponce registerUserAccount(@ModelAttribute("userForm") @Validated User userForm, BindingResult bindingResult, Model model) {
-        if (bindingResult.hasErrors()) {
-            log.debug("BindingResult in registerUserAccount hasErrors: {}", bindingResult);
-            return new GenericResponce("");
-        }
-        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())){
-            log.debug("Passwords do not match : passwordConfirmError");
-            return new GenericResponce("passwordError");
-        }
-        if (userService.isExist(userForm.getEmail())){
-            log.debug("User with same email already exists");
-            return new GenericResponce("duplicatedEmailError");
-        }
-        if (validationUtils.isNotValidEmail(userForm.getEmail())){
-            log.debug("Wrong email! Не правильно введен email");
-            return new GenericResponce("notValidEmailError");
-        }
-        userService.regNewAccount(userForm);
-        return new GenericResponce("success");
-    }
-
-    @GetMapping("/activate/{token}")
-    public String activate(Model model, @PathVariable String token, HttpServletRequest request) {
-        userService.activateUser(token, request);
-        return "redirect:/customer";
-    }
 }
