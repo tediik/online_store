eda2d03c4fb64b1fe5c9b510161677e5b01a8d36
Anton <anton.moskvin1@gmail.com>
2020-08-29 19:37:30 +0300

    work

--- src/main/java/com/jm/online_store/config/security/Twitter/TwitterAuth.java
+++ src/main/java/com/jm/online_store/config/security/Twitter/TwitterAuth.java
@@ -1,53 +1,82 @@
 package com.jm.online_store.config.security.Twitter;
 
-import lombok.AllArgsConstructor;
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.model.OAuthRequest;
+import com.github.scribejava.core.model.Response;
+import com.github.scribejava.core.model.Verb;
+import com.github.scribejava.core.oauth.OAuth10aService;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.xml.bind.DatatypeConverter;
+
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Класс OAuth 1.0 авторизации через Twitter API
  */
 @NoArgsConstructor
-@AllArgsConstructor
 @Service
 public class TwitterAuth {
-    protected Integer appId = 18634122;
-    protected String apiKey = "BiCDQUyByE72SHyBkqJPe1wAi";
-    protected String apiKeySecret = "KlJocOjZNnLodCExCNfsQbaz3utxjhOoc7rFuUwncgolAgQnEA";
-    protected String accessToken = "1298275136731258881-nzPoBgZQvRPPyxn778Wmj3p0Jkdn5L";
-    protected String accessTokenSecret = "OCSM0TgL8ymQsshEUx9RAhKRwNzYaY4g4mIqEiLf1QW0y";
 
-    String query = "https://api.twitter.com/oauth/request_token";
+    public static final String PROTECTED_RESOURCE_URL = "https://api.twitter.com/1.1/account/verify_credentials.json";
+    public String code;
+    OAuth1RequestToken requestToken;
+    OAuth10aService service;
 
 
-    public void Http() throws IOException {
-        String webPage = "https://api.twitter.com/oauth/request_token";
-        String name = "admin";
-        String password = "admin";
+    @SuppressWarnings("PMD.SystemPrintln")
+    public String twitterAuth() throws InterruptedException, ExecutionException, IOException {
+        service = new ServiceBuilder("BiCDQUyByE72SHyBkqJPe1wAi")
+                .apiSecret("KlJocOjZNnLodCExCNfsQbaz3utxjhOoc7rFuUwncgolAgQnEA")
+                .callback("http://localhost:9999" + "/oauth1")
+                .build(TwitterApi.instance());
+        final Scanner in = new Scanner(System.in);
 
-        String authString = name + ":" + password;
-        System.out.println("auth string: " + authString);
-        String authEncBytes = DatatypeConverter.printBase64Binary(authString.getBytes());
-        String authStringEnc = new String(authEncBytes);
-        System.out.println("Base64 encoded auth string: " + authStringEnc);
+        // Obtain the Request Token
+        System.out.println("Fetching the Request Token...");
+        requestToken = service.getRequestToken();
+        System.out.println("Got the Request Token!");
+        System.out.println();
 
-
-        URL url = new URL(webPage);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setRequestProperty("Authorization", "Basic " + authStringEnc);
-        urlConnection.connect();
-
-        System.out.println(urlConnection.getRequestProperty("Authorization"));
+        System.out.println("Now go and authorize ScribeJava here:");
+        return service.getAuthorizationUrl(requestToken);
     }
 
+    public void getAccessToken(String token) throws InterruptedException, ExecutionException, IOException {
+        final String oauthVerifier = code;
 
+        // Trade the Request Token and Verifier for the Access Token
+        System.out.println("Trading the Request Token for an Access Token...");
+        final OAuth1AccessToken accessToken = service.getAccessToken(requestToken, oauthVerifier);
+        System.out.println("Got the Access Token!");
+        System.out.println("(The raw response looks like this: " + accessToken.getRawResponse() + "')");
+        System.out.println();
+
+
+//
+//        // Now let's go and ask for a protected resource!
+//        System.out.println("Now we're going to access a protected resource...");
+//        final OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+//        service.signRequest(accessToken, request);
+//        try (Response response = service.execute(request)) {
+//            System.out.println("Got it! Lets see what we found...");
+//            System.out.println();
+//            System.out.println(response.getBody());
+//        }
+//        System.out.println();
+//        System.out.println("That's it man! Go and build something awesome with ScribeJava! :)");
+
+    }
+
+    public void codeResp(String code) {
+        this.code = code;
+    }
 }
 
 
--- src/main/java/com/jm/online_store/controller/simple/LoginController.java
+++ src/main/java/com/jm/online_store/controller/simple/LoginController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 @AllArgsConstructor
 @Controller
 @RequestMapping("/")
@@ -23,14 +26,23 @@ public class LoginController {
         return "redirect:/";
     }
 
+    @GetMapping("/oauth1")
+    public String oAuthTwitter(@RequestParam String code) throws InterruptedException, ExecutionException, IOException {
+        System.out.println("CODE!!!!!!!!");
+        twitterAuth.getAccessToken(code);
+        return "redirect:/";
+    }
+
     @GetMapping(value = "/login")
-    public String loginPage(Model model) {
+    public String loginPage(Model model) throws InterruptedException, ExecutionException, IOException {
         String authUrlOK = oAuth2Odnoklassniki.getAuthorizationUrl();
-        String twitterUrl = twitterAuth.getAuthorizationUrl();
         model.addAttribute("authUrlOK", authUrlOK);
+
+        String twitterUrl = twitterAuth.twitterAuth();
         model.addAttribute("twitterUrl", twitterUrl);
         return "login";
     }
+
 
     @GetMapping("/denied")
     public String deniedPage() {
