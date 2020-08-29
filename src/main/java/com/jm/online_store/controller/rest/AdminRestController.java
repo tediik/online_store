316abdde9ec57a8577655f337041cb2eb3cbaf21
Dmitry Shishkin <shishkindima@gmail.com>
2020-08-29 12:05:35 +0300

    added admin rest controller

--- src/main/java/com/jm/online_store/controller/rest/AdminRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/AdminRestController.java
@@ -1,17 +1,25 @@
 package com.jm.online_store.controller.rest;
 
+import com.jm.online_store.dto.UserRequestDto;
 import com.jm.online_store.model.User;
 import com.jm.online_store.service.interf.UserService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping(value = "/api/admin")
@@ -20,15 +28,56 @@ public class AdminRestController {
     private final UserService userService;
 
     @GetMapping(value = "/authUser")
-    public ResponseEntity<User> showAuthUserInfo(Authentication authentication){
+    public ResponseEntity<User> showAuthUserInfo(Authentication authentication) {
         User principal = (User) authentication.getPrincipal();
-        User authUser = userService.getOne(principal.getId());
+        User authUser = userService.findById(principal.getId()).get();
         return new ResponseEntity<>(authUser, HttpStatus.OK);
     }
 
     @GetMapping(value = "/allUsers")
-    public ResponseEntity<List<User>> getAllUsersList(){
+    public ResponseEntity<List<User>> getAllUsersList() {
         List<User> allUsers = userService.findAll();
+        if (allUsers == null) {
+            log.info("There are no users in db");
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
         return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/users/{id}")
+    public ResponseEntity<User> getUserInfo(@PathVariable Long id) {
+        if (userService.findById(id).isEmpty()) {
+            log.info("User with id: {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        User user = userService.findById(id).get();
+        log.info("User with id: {} found, email is: {}", id, user.getEmail());
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        try {
+            userService.deleteByID(id);
+        } catch (IllegalArgumentException e) {
+            log.info("There is no user with id: {}", id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        log.info("User with id: {}, was deleted successfully", id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> editUser(@RequestBody User user) {
+        if (userService.findById(user.getId()).isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        userService.updateUserFromAdminPage(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addNewUser(@RequestBody User newUser) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
