ffe837301a7aa3d6a359807d2f51eec1a631f5fc
Dmitry Shishkin <shishkindima@gmail.com>
2020-08-31 22:29:23 +0300

    added stockDetailsPageRender.js

--- src/main/resources/static/js/stockDetailsPageRender.js
+++ src/main/resources/static/js/stockDetailsPageRender.js
@@ -1,9 +1,47 @@
-const customerUrl = "/api/customer"
-let headers = new Headers()
-headers.append("")
+/**
+ * stockId for tests
+ * @type {number}
+ */
+let stockId = 1
 
-function renderStockDetailsPage(stockId) {
+const stockApiUrl = "/api/stock/"
+let myHeaders = new Headers()
+myHeaders.append('Content-type', 'application/json; charset=UTF-8')
 
+
+function handleFacebookShareButton() {
+    fetch(stockApiUrl + stockId, {
+        headers: myHeaders,
+        method: 'POST'
+    }).then(response => response.text()).then(text => console.log(text))
 }
 
-renderStockDetailsPage(stockId)
+function renderStockDetailPage(stockFromDb) {
+    $('#stockHeader').append(`
+        ${stockFromDb.stockTitle}
+    `)
+    $('#stockBody').append(`
+    ${stockFromDb.stockText}
+    `)
+}
+
+/**
+ * function makes fetch to customer Rest controller, receives stock from db in json
+ * and start render page by renderStockDetailPage function
+ * @param stockId - id of stock to render
+ */
+function fetchStockDetailsPage(stockId) {
+    fetch(stockApiUrl + stockId, {
+        headers: myHeaders,
+        method: 'GET'
+    }).then(function (response) {
+        if (response.status !== 200) {
+            // пишем код если респонс не 200
+        } else {
+            response.json().then(stockFromDb => renderStockDetailPage(stockFromDb))
+        }
+    }).catch(error => console.log(error))
+}
+
+fetchStockDetailsPage(stockId)
+
