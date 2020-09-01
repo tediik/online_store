d7784127cc63c005fb7eddd287e00fe32ecd086c
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-01 17:23:30 +0300

    added ResponseEntity<List<SharedStock>> getQuantity()

--- src/main/java/com/jm/online_store/controller/rest/SharedStockRest.java
+++ src/main/java/com/jm/online_store/controller/rest/SharedStockRest.java
@@ -8,10 +8,13 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -30,5 +33,10 @@ public class SharedStockRest {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SharedStock>> getQuantity() {
+        return ResponseEntity.ok(sharedStockService.findAll());
     }
 }
