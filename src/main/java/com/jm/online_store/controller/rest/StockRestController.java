f809c05b3d90a5e4337bc6efdf8ae6f1040b5f89
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-03 16:21:40 +0300

    refactor

--- src/main/java/com/jm/online_store/controller/rest/StockRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/StockRestController.java
@@ -2,16 +2,20 @@ package com.jm.online_store.controller.rest;
 
 import com.jm.online_store.exception.StockNotFoundException;
 import com.jm.online_store.model.Stock;
-import com.jm.online_store.service.interf.SharedStockService;
 import com.jm.online_store.service.interf.StockService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @AllArgsConstructor
@@ -19,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/stock")
 public class StockRestController {
     private final StockService stockService;
-    private final SharedStockService sharedStockService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Stock> getStockById(@PathVariable Long id) {
@@ -29,8 +32,74 @@ public class StockRestController {
         } catch (StockNotFoundException e) {
             return ResponseEntity.notFound().build();
         }
-//        return ResponseEntity.ok(requestedStock);
-        return new ResponseEntity<>(requestedStock, HttpStatus.OK);
+        return ResponseEntity.ok(requestedStock);
     }
+
+    @GetMapping("/allStocks")
+    public ResponseEntity<List<Stock>> getAllStocks() {
+        List<Stock> allStocks;
+        try {
+            allStocks = stockService.findAll();
+        } catch (StockNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(allStocks);
+    }
+
+    @GetMapping("/currentStocks")
+    public ResponseEntity<List<Stock>> getCurrentStocks() {
+        List<Stock> currentStocks;
+        try {
+            currentStocks = stockService.findCurrentStocks();
+        } catch (StockNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(currentStocks);
+    }
+
+    @GetMapping("/futureStocks")
+    public ResponseEntity<List<Stock>> getFutureStocks() {
+        List<Stock> futureStocks;
+        try {
+            futureStocks = stockService.findFutureStocks();
+        } catch (StockNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(futureStocks);
+    }
+
+    @GetMapping("/pastStocks")
+    public ResponseEntity<List<Stock>> getPastStocks() {
+        List<Stock> currentStocks;
+        try {
+            currentStocks = stockService.findPastStocks();
+        } catch (StockNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(currentStocks);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewStock(@RequestBody Stock stock) {
+        stockService.addStock(stock);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteStock(@PathVariable Long id) {
+        stockService.deleteStockById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<String> modifyStock(@RequestBody Stock stock) {
+        try {
+            stockService.updateStock(stock);
+        } catch (StockNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
 
 }
