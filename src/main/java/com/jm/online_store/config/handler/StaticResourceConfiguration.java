adc5599a1333aa7cad222e896c363f7e4828212d
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2020-09-03 21:49:39 +0300

    Update StaticResourceConfiguration.java

--- src/main/java/com/jm/online_store/config/handler/StaticResourceConfiguration.java
+++ src/main/java/com/jm/online_store/config/handler/StaticResourceConfiguration.java
@@ -2,7 +2,6 @@ package com.jm.online_store.config.handler;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
