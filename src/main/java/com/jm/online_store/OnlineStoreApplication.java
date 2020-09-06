ec8df6525e58394fec802ca37cc750ecfbf0fdbd
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-06 14:10:33 +0300

    done

--- src/main/java/com/jm/online_store/OnlineStoreApplication.java
+++ src/main/java/com/jm/online_store/OnlineStoreApplication.java
@@ -1,14 +1,11 @@
 package com.jm.online_store;
 
-import com.jm.online_store.config.YamlPropertySourceFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication
-@PropertySource(value = "classpath:security_oauth.yml", factory = YamlPropertySourceFactory.class)
 public class OnlineStoreApplication {
 
     public static void main(String[] args) {
