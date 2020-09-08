350a01b37561728d3062f4f7ca0ccc23a329a317
Yuri <juran228@gmail.com>
2020-09-08 11:34:28 +0100

    @Repository annotation

--- src/main/java/com/jm/online_store/repository/ProductCommentRepository.java
+++ src/main/java/com/jm/online_store/repository/ProductCommentRepository.java
@@ -2,6 +2,8 @@ package com.jm.online_store.repository;
 
 import com.jm.online_store.model.ProductComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductCommentRepository extends JpaRepository<ProductComment, Long> {
 }
