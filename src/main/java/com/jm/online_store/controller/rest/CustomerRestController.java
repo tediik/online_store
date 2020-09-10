05239fc358df3b4c86df0d4b889188113e3d7e2f
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2020-09-10 15:32:07 +0300

    Update CustomerRestController.java

--- src/main/java/com/jm/online_store/controller/rest/CustomerRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/CustomerRestController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
