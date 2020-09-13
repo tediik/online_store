ce7a27408d8e6da168c26ba1fc5bfd3a3e8d9659
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-13 22:50:28 +0300

    temp

--- src/main/java/com/jm/online_store/config/scheduling/SchedulingConfiguration.java
+++ src/main/java/com/jm/online_store/config/scheduling/SchedulingConfiguration.java
@@ -1,11 +1,10 @@
 package com.jm.online_store.config.scheduling;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
-@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
+//@ConditionalOnProperty(name = "scheduling.enabled", matchIfMissing = true)
 public class SchedulingConfiguration {
 }
