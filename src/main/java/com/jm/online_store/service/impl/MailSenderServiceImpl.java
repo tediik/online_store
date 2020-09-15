16da336295d46f63a289af7aae185687965be43d
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-15 13:50:56 +0300

    added public void sendHtmlMessage(String emailTo, String subject, String htmlBody, String emailType) to MailSenderService

--- src/main/java/com/jm/online_store/service/impl/MailSenderServiceImpl.java
+++ src/main/java/com/jm/online_store/service/impl/MailSenderServiceImpl.java
@@ -6,7 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Slf4j
 @Service
@@ -25,7 +29,19 @@ public class MailSenderServiceImpl implements MailSenderService {
         mailMessage.setTo(emailTo);
         mailMessage.setSubject(subject);
         mailMessage.setText(message);
-        log.info("{} email was sent from {} to {} with subject {} and message {}",emailType, username, emailTo, subject, mailMessage);
+        log.info("{} email was sent from {} to {} with subject {} and message {}", emailType, username, emailTo, subject, mailMessage);
         mailSender.send(mailMessage);
+    }
+
+    @Override
+    public void sendHtmlMessage(String emailTo, String subject, String htmlBody, String emailType) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        mimeMessage.setFrom(username);
+        helper.setTo(emailTo);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        log.info("{} email was sent from {} to {} with subject {} and message {}", emailType, username, emailTo, subject, mimeMessage);
+        mailSender.send(mimeMessage);
     }
 }
