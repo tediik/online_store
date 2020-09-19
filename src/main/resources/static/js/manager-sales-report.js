49e0127b75192bff4b62340064116f8206061e5b
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-19 13:32:53 +0300

    added renderSalesTable, and fetch functions

--- src/main/resources/static/js/manager-sales-report.js
+++ src/main/resources/static/js/manager-sales-report.js
@@ -1,20 +1,15 @@
 let myHeaders = new Headers()
 myHeaders.append('Content-type', 'application/json; charset=UTF-8')
 
-let managerSalesApiUrl = "/manager/api/sharedStock"
+let managerSalesApiUrl = "/api/manager/sales"
 
 $(document).ready(function () {
     dateRangePickerForSalesReport()
 })
 
-// function dateRangePickerForSalesReport(){
-//     $('input[name="dateRange"]').daterangepicker({
-//         opens: 'left'
-//     }, function(start, end, label) {
-//         console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-//     });
-// }
-
+/**
+ * function setting up datepicker field
+ */
 function dateRangePickerForSalesReport() {
     let start = moment().subtract(29, 'days');
     let end = moment();
@@ -39,8 +34,88 @@ function dateRangePickerForSalesReport() {
     cb(start, end);
 }
 
-$('#managerSalesReportRange').on('apply.daterangepicker', function(ev, picker) {
-    fetch()
+/**
+ * fetch GET request to server to receive list of sales for custom range date
+ */
+$('#managerSalesReportRange').on('apply.daterangepicker', function (ev, picker) {
+    let StartDate = picker.startDate.format('YYYY-MM-DD')
+    let EndDate = picker.endDate.format('YYYY-MM-DD')
+    fetch(managerSalesApiUrl + `?stringStartDate=${StartDate}&stringEndDate=${EndDate}`)
+        .then(function (response){
+            if (response.status === 200){
+                response.json().then(sales => renderSalesTable(sales))
+            } else {
+                infoMessage('#infoMessageDiv', 'За указанный период продаж не найдено', 'error')
+            }
+        })
     console.log(picker.startDate.format('YYYY-MM-DD'));
     console.log(picker.endDate.format('YYYY-MM-DD'));
 });
+
+/**
+ * Function that renders sales report table
+ * @param sales
+ */
+function renderSalesTable(sales){
+    let salesTableBody = $('#salesTableBody')
+    salesTableBody.empty()
+    for (let i = 0; i < sales.length; i++){
+        let row = `<tr id=tr-${sales[i].id}>
+                        <th scope="row">${sales[i].orderNumber}</th>
+                        <td>${sales[i].userEmail}</td>
+                        <td>${sales[i].customerInitials}</td>
+                        <td>${sales[i].purchaseDate}</td>
+                        <td>${sales[i].quantity}</td>
+                        <td>${sales[i].listOfProducts}</td>
+                        <td>${sales[i].orderSummaryPrice}</td>
+                    </tr>`
+        salesTableBody.append(row)
+    }
+}
+
+/**
+ * function that sorts the table by header
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    const getSort = ({ target }) => {
+        const order = (target.dataset.order = -(target.dataset.order || -1));
+        const index = [...target.parentNode.cells].indexOf(target);
+        const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+        const comparator = (index, order) => (a, b) => order * collator.compare(
+            a.children[index].innerHTML,
+            b.children[index].innerHTML
+        );
+
+        for(const tBody of target.closest('table').tBodies)
+            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+        for(const cell of target.parentNode.cells)
+            cell.classList.toggle('sorted', cell === target);
+    };
+    document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
+});
+
+function infoMessage(inputField, text, messageStatus) {
+    let successMessage = `<div class="alert text-center alert-success alert-dismissible" role="alert">
+                            <strong>${text}</strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                       </div>`
+    let alertMessage = `<div class="alert text-center alert-danger alert-dismissible" role="alert">
+                            <strong>${text}</strong>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                       </div>`
+    let message = ''
+
+    if (messageStatus === "success") {
+        message = successMessage
+    } else if (message === "error") {
+        message = alertMessage;
+    }
+
+    document.querySelector(`${inputField}`).innerHTML = message
+    window.setTimeout(function () {
+        $('.alert').alert('close');
+    }, 3000)
+}
