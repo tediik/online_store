1ac47e457f9841fee056eca947ef6b56e1a7f5ef
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-22 16:03:58 +0300

    added ExceptionHandler to controller

--- src/main/java/com/jm/online_store/controller/rest/SchedulingTaskController.java
+++ src/main/java/com/jm/online_store/controller/rest/SchedulingTaskController.java
@@ -1,6 +1,5 @@
 package com.jm.online_store.controller.rest;
 
-import com.jm.online_store.exception.StockNotFoundException;
 import com.jm.online_store.exception.TaskNotFoundException;
 import com.jm.online_store.model.TaskSettings;
 import com.jm.online_store.service.interf.StockMailDistributionTask;
@@ -8,6 +7,7 @@ import com.jm.online_store.service.interf.TaskSchedulingService;
 import com.jm.online_store.service.interf.TaskSettingsService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,35 +23,27 @@ public class SchedulingTaskController {
     private final TaskSettingsService taskSettingsService;
     private final StockMailDistributionTask stockMailDistributionTask;
 
+    @ExceptionHandler({TaskNotFoundException.class})
+    public ResponseEntity<String> taskNotFoundExceptionHandler() {
+        return ResponseEntity.notFound().build();
+    }
+
     @PostMapping("/stockMailDistribution/start")
     public ResponseEntity<TaskSettings> startMailDistributionTask(@RequestBody TaskSettings taskSettings) {
-        try {
-            taskSettingsService.updateTask(taskSettings);
-            schedulingService.addTaskToScheduler(taskSettings, stockMailDistributionTask);
-            return ResponseEntity.ok().build();
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
+        taskSettingsService.updateTask(taskSettings);
+        schedulingService.addTaskToScheduler(taskSettings, stockMailDistributionTask);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("/stockMailDistribution/stop")
     public ResponseEntity<TaskSettings> stopMailDistributionTask(@RequestBody TaskSettings taskSettings) {
-        try {
-            taskSettingsService.updateTask(taskSettings);
-            schedulingService.removeTaskFromScheduler(taskSettings.getId());
-            return ResponseEntity.ok().build();
-        } catch (TaskNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
+        taskSettingsService.updateTask(taskSettings);
+        schedulingService.removeTaskFromScheduler(taskSettings.getId());
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{taskName}")
     public ResponseEntity<TaskSettings> getMailDistributionSettings(@PathVariable String taskName) {
-        try {
-            TaskSettings taskSettings = taskSettingsService.findTaskByName(taskName);
-            return ResponseEntity.ok(taskSettings);
-        } catch (StockNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.ok(taskSettingsService.findTaskByName(taskName));
     }
 }
