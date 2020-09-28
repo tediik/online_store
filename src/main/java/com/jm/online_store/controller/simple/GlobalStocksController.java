2a026774702f411d2fd183efc867ae392baee4ec
DeMaN <=>
2020-09-29 01:44:49 +0300

    Доделал всю логику с акциями(теперь показывает все акции и можно посмотреть каждую)

--- src/main/java/com/jm/online_store/controller/simple/GlobalStocksController.java
+++ src/main/java/com/jm/online_store/controller/simple/GlobalStocksController.java
@@ -1,16 +1,28 @@
 package com.jm.online_store.controller.simple;
 
+import com.jm.online_store.model.Stock;
+import com.jm.online_store.service.impl.StockServiceImpl;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@AllArgsConstructor
 @Controller
 @RequestMapping("/global")
 public class GlobalStocksController {
 
-    //временная заглушка пока нет акций на главной
-    @GetMapping("/stockDetails")
-    public String showStockDetailsPage(){
+    private final StockServiceImpl stockService;
+
+    @GetMapping("/stockDetails/{id}")
+    public String showStockDetailsPage(@PathVariable(value = "id") Long id, Model model) {
+        if (stockService.findStockById(id) == null) {
+            return "redirect:/stocks";
+        }
+        Stock stock = stockService.findStockById(id);
+        model.addAttribute("stock", stock);
         return "stockGlobalPage";
     }
 }
