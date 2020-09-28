7ed157df246da5ee34f3080e339cb2a9ad0f0c6e
Dmitry Shishkin <shishkindima@gmail.com>
2020-09-28 14:21:41 +0300

    added void addProductToBasket(User user, Long id); method

--- src/main/java/com/jm/online_store/service/impl/BasketServiceImpl.java
+++ src/main/java/com/jm/online_store/service/impl/BasketServiceImpl.java
@@ -1,15 +1,24 @@
 package com.jm.online_store.service.impl;
 
+import com.jm.online_store.exception.ProductNotFoundException;
+import com.jm.online_store.model.Product;
 import com.jm.online_store.model.SubBasket;
+import com.jm.online_store.model.User;
 import com.jm.online_store.repository.BasketRepository;
 import com.jm.online_store.service.interf.BasketService;
+import com.jm.online_store.service.interf.ProductService;
+import com.jm.online_store.service.interf.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BasketServiceImpl implements BasketService {
     private final BasketRepository basketRepository;
+    private final ProductService productService;
+    private final UserService userService;
 
     @Override
     public SubBasket findBasketById(Long idBasket) {
@@ -29,5 +38,20 @@ public class BasketServiceImpl implements BasketService {
     @Override
     public void deleteBasket(SubBasket subBasket) {
         basketRepository.delete(subBasket);
+    }
+
+    @Override
+    public void addProductToBasket(User user, Long id) {
+        User userToModify = userService.findById(user.getId()).get();
+        Product productToAdd = productService
+                .findProductById(id)
+                .orElseThrow(ProductNotFoundException::new);
+        SubBasket subBasket = SubBasket.builder()
+                .product(productToAdd)
+                .count(1)
+                .build();
+        List<SubBasket> userBasket = userToModify.getUserBasket();
+        userBasket.add(subBasket);
+
     }
 }
--- src/main/java/com/jm/online_store/service/interf/BasketService.java
+++ src/main/java/com/jm/online_store/service/interf/BasketService.java
@@ -1,10 +1,13 @@
 package com.jm.online_store.service.interf;
 
 import com.jm.online_store.model.SubBasket;
+import com.jm.online_store.model.User;
 
 public interface BasketService {
     SubBasket addBasket(SubBasket subBasket);
     SubBasket findBasketById(Long idBasket);
     SubBasket updateBasket(SubBasket subBasket);
     void deleteBasket(SubBasket subBasket);
+
+    void addProductToBasket(User user, Long id);
 }
