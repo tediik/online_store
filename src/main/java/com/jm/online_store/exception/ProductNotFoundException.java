748368264e7534a2f5e5e634f94b1e5ee3118f26
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2020-09-30 13:33:26 +0300

    Update ProductNotFoundException.java

--- src/main/java/com/jm/online_store/exception/ProductNotFoundException.java
+++ src/main/java/com/jm/online_store/exception/ProductNotFoundException.java
@@ -1,7 +1,7 @@
 package com.jm.online_store.exception;
 
 public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(){
+    public ProductNotFoundException() {
         super("Product Not Found");
     }
 }
