4e89e81cf8d34a498b5320b5f709500889db12ed
Evgeniy Rubtsov <zhekarubtsov95@gmail.com>
2020-09-30 14:54:21 +0300

    added icon for favourite product

--- src/main/resources/static/js/productPage.js
+++ src/main/resources/static/js/productPage.js
@@ -63,6 +63,17 @@ async function fillAboutProduct(data) {
     let productName = document.getElementById('productName');
     $(productName).append(`<br><h2 class="font-weight-normal">${data.product}</h2>`);
     $(productName).append(`<br><h5 class="font-weight-normal">Оценка товара: ${data.rating} из 5</h5>`);
+    $("#favouriteContainer").append(`<div id="favoriteLabel"><h5 class="font-weight-normal my-2">&ensp;В избранное </h5></div>
+    <svg width="640" height="480" viewbox="0 0 640 480" id="heartSvg">
+        <path id="heart"
+    d="m219.28949,21.827393c-66.240005,0 -119.999954,53.76001 -119.999954,
+    120c0,134.755524 135.933151,170.08728 228.562454,303.308044c87.574219,
+        -132.403381 228.5625,-172.854584 228.5625,-303.308044c0,-66.23999
+    -53.759888,-120 -120,-120c-48.047913,0 -89.401611,28.370422 -108.5625,
+        69.1875c-19.160797,-40.817078 -60.514496,-69.1875 -108.5625,-69.1875z"
+    onclick="addToFavourite()"/>
+        </svg>`);
+
 
     // заполнение вкладки с описанием продукта
     let description = document.getElementById('text-description');
@@ -77,5 +88,20 @@ async function fillAboutProduct(data) {
                        </tr>`;
             $(specifications).append(content);
         }
+    }
+}
+
+function addToFavourite() {
+    if($("path").is('[class="filled"]')) {
+        $("#heart").toggleClass("filled");
+        $("#favoriteLabel").empty().append(`<h5 class="font-weight-normal my-2">&ensp;В избранное</h5>`)
+    } else {
+        fetch("/customer/favouritesGoods", {
+            method: "PUT",
+            body: productIdFromPath,
+            headers: {"Content-Type": "application/json; charset=utf-8"}
+        });
+        $("#heart").toggleClass("filled");
+        $("#favoriteLabel").empty().append(`<h5 class="font-weight-normal my-2">&ensp;Удалить</h5>`)
     }
 }
