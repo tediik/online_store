25af35c0da6a33b8877da2b926a6e50ca034a43a
Dmitry Shishkin <shishkindima@gmail.com>
2020-10-02 10:59:32 +0300

    modified Feedback entity

--- src/main/java/com/jm/online_store/model/Feedback.java
+++ src/main/java/com/jm/online_store/model/Feedback.java
@@ -1,8 +1,9 @@
 package com.jm.online_store.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -11,22 +12,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import java.time.LocalDateTime;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Feedback {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne(fetch = FetchType.LAZY)
-    private FeedbackTopic topic;
+    private Topic topic;
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-    @Column(name = "feedback_post_date")
     private LocalDateTime feedbackPostDate;
-    @Column(name = "status")
     @Enumerated(EnumType.STRING)
     private Status status;
 
