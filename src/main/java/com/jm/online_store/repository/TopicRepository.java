e5ecc73cb589083fdb61b1e8fc51cb83f1a81039
Dmitry Shishkin <shishkindima@gmail.com>
2020-10-05 14:30:53 +0300

    some changes to topic repostory

--- src/main/java/com/jm/online_store/repository/TopicRepository.java
+++ src/main/java/com/jm/online_store/repository/TopicRepository.java
@@ -3,19 +3,12 @@ package com.jm.online_store.repository;
 import com.jm.online_store.model.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
     @Query("SELECT DISTINCT t.topicCategory FROM Topic t")
     List<String> findAllCategories();
 
-    @Query("FROM Topic t WHERE t.topicCategory = :category")
-    List<Topic> findTopicsByCategory(@Param("category") String category);
-
-    Topic findTopicByTopicName(String topicName);
-
-    Topic findTopicById(Long id);
+    List<Topic> findTopicByTopicCategoryEquals(String category);
 }
