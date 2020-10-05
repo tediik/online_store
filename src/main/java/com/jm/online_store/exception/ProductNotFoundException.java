22773d7cf2fd96f21e56d9d823fc34e8de4d3850
Dmitry Shishkin <shishkindima@gmail.com>
2020-10-05 10:54:27 +0300

    code style

--- src/main/java/com/jm/online_store/exception/ProductNotFoundException.java
+++ src/main/java/com/jm/online_store/exception/ProductNotFoundException.java
@@ -3,6 +3,5 @@ package com.jm.online_store.exception;
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException() {
         super("Product Not Found");
->>>>>>> dev
     }
 }
