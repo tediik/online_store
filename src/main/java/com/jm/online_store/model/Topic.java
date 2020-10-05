79e6b316a014fe8fe41fcc9fe3dd3cd0a003b45e
Dmitry Shishkin <shishkindima@gmail.com>
2020-10-05 14:29:48 +0300

    added @JsonIdentityInfo

--- src/main/java/com/jm/online_store/model/Topic.java
+++ src/main/java/com/jm/online_store/model/Topic.java
@@ -1,9 +1,16 @@
 package com.jm.online_store.model;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,10 +21,14 @@ import javax.persistence.OneToMany;
 import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIdentityInfo(
+        generator = ObjectIdGenerators.PropertyGenerator.class,
+        property = "id")
 public class Topic {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
