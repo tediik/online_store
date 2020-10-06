59bb531694e1b8581944bcd2d69015a699e9c924
Dmitry Shishkin <shishkindima@gmail.com>
2020-10-06 10:48:47 +0300

    refactor service layer according new relation feedback and user

--- src/main/java/com/jm/online_store/service/impl/FeedbackServiceImpl.java
+++ src/main/java/com/jm/online_store/service/impl/FeedbackServiceImpl.java
@@ -2,7 +2,6 @@ package com.jm.online_store.service.impl;
 
 import com.jm.online_store.model.Feedback;
 import com.jm.online_store.repository.FeedbackRepository;
-import com.jm.online_store.repository.TopicRepository;
 import com.jm.online_store.service.interf.FeedbackService;
 import com.jm.online_store.service.interf.UserService;
 import lombok.AllArgsConstructor;
@@ -15,14 +14,19 @@ import java.time.temporal.ChronoUnit;
 @AllArgsConstructor
 public class FeedbackServiceImpl implements FeedbackService {
     private final FeedbackRepository feedbackRepository;
-    private final TopicRepository topicRepository;
     private final UserService userService;
 
+    /**
+     * Method saves feedback from current authenticated user
+     * with current time {@link LocalDateTime} trimmed to seconds and default status
+     *  for new feedbacks TO_DO
+     * @param feedback - {@link Feedback} to save
+     */
     @Override
     public void addFeedbackFromDto(Feedback feedback) {
+        feedback.setUser(userService.getCurrentLoggedInUser());
         feedback.setFeedbackPostDate(LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
         feedback.setStatus(Feedback.Status.TO_DO);
-
         feedbackRepository.save(feedback);
     }
 }
