b82f82cbe95663d28c5f15106ca05ee45041ce1b
Vyacheslav-Ko <Ignition1978@gmail.com>
2020-10-08 15:07:01 +0500

    Удалена возможность оформить заказ с пустой корзиной

--- src/main/resources/static/js/busket.js
+++ src/main/resources/static/js/busket.js
@@ -58,7 +58,21 @@ async function fillBusket() {
 
     $('#countBasketGoods').append(countGoods + "шт.");
     $('#sumBasketGoods').append(sumBasket);
+
+    //if basket is empty
+    if (countGoods == 0) {
+        let emptyBasket = "<tr align='center'> <td > Корзина сейчас пуста,</br>но вы можете перейти на главную страницу или в каталог товаров</td></tr>";
+        emptyBasket += "<tr > <td ></td></tr>";
+        emptyBasket += "<tr align='center'><td >" +
+            "    <a class='btn btn-outline-primary mr-2' role='button' onclick='' href='/'>На главную страницу</a>" +
+            "    <a class='btn btn-outline-danger ml-2' role='button' onclick='' href='#'>Перейдите в каталог</a>" +
+            "    </td></tr>";
+        let buttonBuy = "<button class=\"btn btn-lg btn-outline-primary btn-block\" data-toggle=\"modal\" data-target=\"#orderModalWindow\" onclick=\"userAdresses()\" disabled>Корзина пуста</button>"
+        $(basketGoodsJson).append(emptyBasket);
+        $('#buttonBuyInBasket').empty().append(buttonBuy);
+    }
 }
+
 
 async function deleteBasket(id) {
     await fetch("/customer/busketGoods", {
