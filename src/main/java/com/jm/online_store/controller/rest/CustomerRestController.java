122a7e067274c8f71b0aec32d0d73ac7704e2211
DeMaN <=>
2020-10-23 15:23:38 +0300

    Правки

--- src/main/java/com/jm/online_store/controller/rest/CustomerRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/CustomerRestController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
