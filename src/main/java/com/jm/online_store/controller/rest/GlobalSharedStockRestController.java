113e3c3186fe6c45b6a03aa6ee91054f704394a5
timRybin <tim.rybin@gmail.com>
2020-10-29 22:07:36 +0300

    Убрал неиспользуемые импорты

--- src/main/java/com/jm/online_store/controller/rest/GlobalSharedStockRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/GlobalSharedStockRestController.java
@@ -1,13 +1,11 @@
 package com.jm.online_store.controller.rest;
 
 import com.jm.online_store.model.SharedStock;
-import com.jm.online_store.model.User;
 import com.jm.online_store.service.interf.SharedStockService;
 import com.jm.online_store.service.interf.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
