1e797ab0edf71fd8da4f0140b0225fce39d5c403
timRybin <tim.rybin@gmail.com>
2020-10-29 20:23:52 +0300

    Добавил слушателей событий и соцсетей для 'Поделиться'

--- src/main/resources/static/js/stockDetailsPageRender.js
+++ src/main/resources/static/js/stockDetailsPageRender.js
@@ -13,20 +13,35 @@ $(document).ready(function () {
 /**
  * Share buttons event listeners
  */
-document.getElementById('shareFacebookLink').addEventListener('click', handleShareButton)
-document.getElementById('shareVKLink').addEventListener('click', handleShareButton)
+document.getElementById('shareVKLink').addEventListener('click', handleShareButton);
+document.getElementById('shareFbLink').addEventListener('click', handleShareButton);
+document.getElementById('shareOkLink').addEventListener('click', handleShareButton);
+document.getElementById('shareTwitterLink').addEventListener('click', handleShareButton);
+document.getElementById('shareCopyLink').addEventListener('click', handleShareButton);
 
 /**
  * generates share links
  */
 function generateShareLinks() {
     let encoded = encodeURIComponent(urlToShare)
-    let facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${encoded}`
-    let vkShareLink = `https://vk.com/share.php?url=${urlToShare}`
-    $('#shareFacebookLink').attr("href", facebookShareLink)
-    $('#shareVKLink').attr("href", vkShareLink)
+    let facebookShareLink = `https://www.facebook.com/sharer/sharer.php?u=${encoded}`;
+    let vkShareLink = `https://vk.com/share.php?url=${urlToShare}`;
+    let okShareLink = `https://connect.ok.ru/offer?url=${urlToShare}`;
+    let twitterShareLink = `http://twitter.com/share?&url=${urlToShare}`;
+    $('#shareFbLink').attr("href", facebookShareLink);
+    $('#shareVKLink').attr("href", vkShareLink);
+    $('#shareOkLink').attr("href", okShareLink);
+    $('#shareTwitterLink').attr("href", twitterShareLink);
+    $('#shareCopyLink').attr("href", getCopyLink());
 }
 
+function getCopyLink() {
+    new Clipboard('.copy_link', {
+        text: function () {
+            return urlToShare;
+        }
+    });
+}
 /**
  * function that handles share buttons.
  * Sends POST fetch request to shared stock api
@@ -46,7 +61,8 @@ function handleShareButton() {
         method: 'POST',
         headers: myHeaders,
         body: JSON.stringify(body)
-    }).then(response => response.text()).then(text => console.log(text))
+    }).then(response => response.text())
+        .then(text => console.log(text))
 }
 
 
