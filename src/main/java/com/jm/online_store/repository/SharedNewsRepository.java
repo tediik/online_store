6b357a0d0f0989838bd679dfa3c89331e270db0c
timRybin <tim.rybin@gmail.com>
2020-10-31 03:56:35 +0300

    Добавил аннотацию @Repository

--- src/main/java/com/jm/online_store/repository/SharedNewsRepository.java
+++ src/main/java/com/jm/online_store/repository/SharedNewsRepository.java
@@ -2,6 +2,8 @@ package com.jm.online_store.repository;
 
 import com.jm.online_store.model.SharedNews;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SharedNewsRepository extends JpaRepository<SharedNews, Long> {
 }
