7372882e15fb410a50dae3e558094d9af602fd09
timRybin <tim.rybin@gmail.com>
2020-10-31 16:44:20 +0300

    Добавил отрисовку блока поделиться в соцсетях для newsDetails.html и stockGlobalPage.html. Перенес слушателей событий в $(document).ready(), после отрисовки блока поделиться в соцсетях и генерации ссылок. Добавил toastr, но он не работает:/

--- src/main/resources/static/js/socialNetworksSharer.js
+++ src/main/resources/static/js/socialNetworksSharer.js
@@ -1,21 +1,56 @@
 /**
  * Global variables declaration
  */
-let urlToShare = document.location.href;
+const urlToShare = document.location.href;
+let splitUrl = urlToShare.split('/');
+const id = splitUrl[splitUrl.length - 1];
+
+console.log(id);
+console.log(urlToShare);
 
 $(document).ready(function () {
+    $('head').append('<link href="/static/css/socialShare.css" rel="stylesheet" type="text/css">');
+    renderShareLinks();
     generateShareLinks();
-})
+    document.getElementById('shareVKLink').addEventListener('click', handleShareButton);
+    document.getElementById('shareFbLink').addEventListener('click', handleShareButton);
+    document.getElementById('shareOkLink').addEventListener('click', handleShareButton);
+    document.getElementById('shareTwitterLink').addEventListener('click', handleShareButton);
+    document.getElementById('shareCopyLink').addEventListener('click', handleShareButton);
+    document.getElementById('shareCopyLink').addEventListener('click', copyLink);
+
+});
 
 /**
  * Share buttons event listeners
  */
-document.getElementById('shareVKLink').addEventListener('click', handleShareButton);
-document.getElementById('shareFbLink').addEventListener('click', handleShareButton);
-document.getElementById('shareOkLink').addEventListener('click', handleShareButton);
-document.getElementById('shareTwitterLink').addEventListener('click', handleShareButton);
-document.getElementById('shareCopyLink').addEventListener('click', handleShareButton);
-document.getElementById('shareCopyLink').addEventListener('click', copyLink);
+function renderShareLinks() {
+    let shareSocialNetworksBody = document.querySelector('#insertSocialNetworksBlock');
+    let insertBody = `<div class="buttonForSocialShare">
+                        <a href="#" id="dLabel" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                           data-offset="-125, 40"><img src="/static/img/share/shareButton.svg" alt="shareButton"></a>
+                        <div class="menuForSocialShare">
+                            <div class="dropdown-menu" aria-labelledby="dLabel">
+                                <a class="dropdown-item" id="shareVKLink" data-socialNetworkName="vk" target="_blank">
+                                    <img src="/static/img/share/vk_logo.svg" alt="vk_logo"> ВКонтакте
+                                </a>
+                                <a class="dropdown-item" id="shareFbLink" data-socialNetworkName="facebook" target="_blank">
+                                    <img src="/static/img/share/fb_logo.svg" alt="fb_logo"> Facebook
+                                </a>
+                                <a class="dropdown-item" id="shareOkLink" data-socialNetworkName="odnoklassniki" target="_blank">
+                                    <img src="/static/img/share/ok_logo.svg" alt="ok_logo"> Одноклассники
+                                </a>
+                                <a class="dropdown-item" id="shareTwitterLink" data-socialNetworkName="twitter" target="_blank">
+                                    <img src="/static/img/share/tw_logo.svg" alt="tw_logo"> Twitter
+                                </a>
+                                <a class="dropdown-item" id="shareCopyLink" data-socialNetworkName="copyLink">
+                                    <img src="/static/img/share/copy_link.svg" alt="copy_link"> Скопировать ссылку
+                                </a>
+                            </div>
+                        </div>
+                    </div>`;
+    shareSocialNetworksBody.insertAdjacentHTML("beforeend", insertBody);
+}
 
 
 /**
@@ -40,6 +75,7 @@ function copyLink() {
     link.select();
     document.execCommand('copy');
     document.body.removeChild(link);
+    toastr.error("My name is Inigo Montoya. You killed my father. Prepare to die!");
 }
 
 /**
@@ -51,7 +87,6 @@ function handleShareButton() {
     let sharedStockApiUrl;
     let body;
     let socialNetworkName = $(this).attr('data-socialNetworkName');
-    let id = $(this).attr('data-id');
 
     if (urlToShare.includes('news')) {
         sharedStockApiUrl = '/global/api/sharedNews/';
