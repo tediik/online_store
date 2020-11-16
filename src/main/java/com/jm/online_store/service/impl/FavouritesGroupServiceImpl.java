a9d54776888e6604fb852fd497e3dd2e937f2f3d
KuGa-Do <karukaleksandr@icloud.com>
2020-11-16 16:48:24 +0300

    пробелы для чтения

--- src/main/java/com/jm/online_store/service/impl/FavouritesGroupServiceImpl.java
+++ src/main/java/com/jm/online_store/service/impl/FavouritesGroupServiceImpl.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @Service
 @AllArgsConstructor
 public class FavouritesGroupServiceImpl implements FavouritesGroupService {
+
     private final FavouritesGroupRepository favouritesGroupRepository;
 
     @Override
--- src/main/java/com/jm/online_store/service/interf/FavouriteGoodsService.java
+++ src/main/java/com/jm/online_store/service/interf/FavouriteGoodsService.java
@@ -6,7 +6,10 @@ import com.jm.online_store.model.User;
 import java.util.Set;
 
 public interface FavouriteGoodsService {
+
     void deleteFromFavouriteGoods(Long id, User currentUser);
+
     void addToFavouriteGoods(Long id, User currentUser);
+
     Set<Product> getFavouriteGoods(User currentUser);
 }
