8a032d25f7df673bcd5952f9d58123c5ad7c7e99
timRybin <tim.rybin@gmail.com>
2020-11-25 07:31:00 +0300

    Изменил статус ответа, если пользователь уже подписан

--- src/main/java/com/jm/online_store/controller/rest/ProductRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/ProductRestController.java
@@ -8,6 +8,7 @@ import com.jm.online_store.service.interf.ProductService;
 import com.jm.online_store.service.interf.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -106,7 +107,7 @@ public class ProductRestController {
         if (productService.addNewSubscriber(body)) {
             return ResponseEntity.ok().build();
         } else {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.ALREADY_REPORTED).build();
         }
     }
 }
