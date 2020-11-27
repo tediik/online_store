7036080758816aff73202572128d369f90d08a2f
MaximMyDev <max.monarch@gmail.com>
2020-11-27 14:39:20 +0600

    на стр менеджера поменял ссылку на профиль. добавил в контроллер маппинг.

--- src/main/java/com/jm/online_store/controller/simple/ManagerController.java
+++ src/main/java/com/jm/online_store/controller/simple/ManagerController.java
@@ -1,12 +1,18 @@
 package com.jm.online_store.controller.simple;
 
+import com.jm.online_store.model.User;
+import com.jm.online_store.service.interf.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/manager")
 public class ManagerController {
+    private final UserService userService;
 
     @GetMapping
     public String getManagerPage() {
@@ -36,5 +42,15 @@ public class ManagerController {
     @GetMapping("/feedback")
     public String getFeedbacks() {
         return "managerFeedback";
+    }
+
+    /*
+     добавил маппинг для отображения профиля на странице менеджера
+     */
+    @GetMapping("/profile")
+    public String getPersonalInfo(Model model) {
+        User user = userService.getCurrentLoggedInUser();
+        model.addAttribute("user", user);
+        return "managerProfile";
     }
 }
