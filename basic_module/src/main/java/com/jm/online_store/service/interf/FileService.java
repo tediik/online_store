8b438ff53aee2c7a063261067b8d2e5a3849d95d
Vladislav Gulyaev <gulyaevv@yahoo.com>
2020-12-17 18:33:32 +0300

    удалил ненужный сервис

--- basic_module/src/main/java/com/jm/online_store/service/interf/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.jm.online_store.service.interf;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface FileService {
-    String uploadFile(MultipartFile file);
-}
