48884901d66aba8bb4287e9e38afa2dfa33769ee
iambme <iambme@mail.ru>
2020-12-22 17:17:44 +0300

    completed migration to PostgreSql

--- src/main/java/com/jm/online_store/model/User.java
+++ src/main/java/com/jm/online_store/model/User.java
@@ -10,22 +10,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
@@ -46,6 +31,7 @@ import java.util.Set;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ApiModel(description =  "Сущность User, расширеяет UserDetails, связана с Role, Product," +
         " FavouritesGroup и Address")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
