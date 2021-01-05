96e3d9e9381a0c91303b3c41034198488d979ff8
Cotharsis <valeev-p@mail.ru>
2021-01-05 19:58:01 +0300

    Сделать тесты для методов из BadWordsServiceImpl

--- src/test/java/com/jm/online_store/service/impl/BadWordsServiceImplTest.java
+++ src/test/java/com/jm/online_store/service/impl/BadWordsServiceImplTest.java
@@ -1,177 +1,60 @@
 package com.jm.online_store.service.impl;
 
 import com.jm.online_store.model.BadWords;
-import com.jm.online_store.model.Product;
-import com.jm.online_store.repository.AddressRepository;
 import com.jm.online_store.repository.BadWordsRepository;
 import com.jm.online_store.service.interf.CommonSettingsService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.jm.online_store.exception.ProductNotFoundException;
-import com.jm.online_store.model.Product;
-import com.jm.online_store.repository.ProductRepository;
-import com.jm.online_store.service.interf.CategoriesService;
-import com.jm.online_store.service.interf.CommonSettingsService;
-import com.jm.online_store.service.interf.EvaluationService;
-import com.jm.online_store.service.interf.MailSenderService;
-import com.jm.online_store.service.interf.ProductCharacteristicService;
-import com.jm.online_store.service.interf.ProductService;
-import com.jm.online_store.service.interf.UserService;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
-import java.util.regex.Pattern;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+@ExtendWith(MockitoExtension.class)
+public class BadWordsServiceImplTest {
 
-class BadWordsServiceImplTest {
-    @InjectMocks
-    BadWordsServiceImpl badWordsService;
-    @Mock
-    private BadWordsRepository badWordsRepository;
-    @Mock
-    private CommonSettingsService commonSettingsService;
-    private List<BadWords> allActiveBWw;
+    private BadWordsRepository badWordsRepository= mock(BadWordsRepository.class);;
+    private CommonSettingsService commonSettingsService = mock( CommonSettingsService.class);
+    private BadWordsServiceImpl badWordsService = new BadWordsServiceImpl(badWordsRepository,commonSettingsService);
+
+    private List<BadWords> allActiveBW;
+    private List<String> expectedBadWords;
     private String incoming;
+    private String incoming2;
 
-    @BeforeEach
-    void init() {
-        allActiveBWw = Arrays.asList(
+
+////перед тем как раскоментировать сделать preparingWordsForImport публичным
+//    @Test
+//    void preparingWordsForImport() {
+//        incoming="Winter,   ,    g, Spring,,      Sum - mer,   Autumn,    765,";
+//        String[] expected= {"Winter", "", "g", "Spring,", "Sum - mer", "Autumn", "765"};
+//        String[] actual = badWordsService.preparingWordsForImport(incoming);
+//        assertArrayEquals(expected, actual);
+//    }
+
+
+    @Test
+    void importWord() {
+        incoming2="Winter, , g, Spring, Summer, Autumn, h";
+        badWordsService.importWord(incoming2);
+        verify(badWordsRepository, times(4)).existsBadWordsByBadword(any(String.class));
+        verify(badWordsRepository, times(4)).save(any(BadWords.class));
+    }
+
+    @Test
+    void CheckComment() {
+        allActiveBW = Arrays.asList(
                 new BadWords("поршивка",true),
                 new BadWords("евошняя",false),
-                new BadWords("Глазница",true),
+                new BadWords("глазница",true),
                 new BadWords("гад",true));
         incoming="lol'Ах ты ПОРШиВка!!!.ббббббр' - говно Закричал поп:'Мне не нужна евошняя Титановая Глазница...GGH5'";
-
+        expectedBadWords = Arrays.asList("поршивка", "евошняя", "глазница");
+        Mockito.when(badWordsService.findAllWordsActive()).thenReturn(allActiveBW);
+        List<String> actual = badWordsService.checkComment(incoming);
+        assertEquals(expectedBadWords, actual);
     }
-
-    @Test
-    void checkComment() {
-        String[] expect={"поршивка", "евошняя", "глазница"};
-        List<String> expected = Arrays.asList(expect);
-
-        String textForCheck = incoming.toLowerCase();
-        System.out.println(textForCheck);
-//        List<BadWords> allActiveBW = Arrays.asList(new BadWords("поршивка",true),new BadWords("евошняя",false),
-//                new BadWords("Глазница",true),new BadWords("гад",true));
-//        when(badWordsRepository.getBadWordsActive()).thenReturn(Optional.ofNullable(allActiveBWw));
-//             Optional<BadWords> optionalProduct = badWordsService.checkComment(incoming);
-//             assertNotNull(optionalProduct);
-//             verify(badWordsRepository, times(1)).findById(product.getId());
-
-        List<BadWords> allActiveBW = badWordsRepository.getBadWordsActive();
-        List<String> foundWords = new ArrayList<>();
-        for (BadWords wordCheck : allActiveBW) {
-            String findMe = wordCheck.getBadword().toLowerCase();
-            if (textForCheck.matches(".*?\\b" + findMe + "\\b.*?")) {
-                foundWords.add(findMe);
-            }
-        }
-//        String[] actual ={"2","3"};
-        System.out.println(foundWords);
-        assertLinesMatch(expected,foundWords);
-
-//        List<String> strings = badWordsService.checkComment(incoming);
-//        System.out.println(strings);
-//
-//        assertArrayEquals(expected,strings.toArray());
-    }
-    /**
-     * Метод принимает текст, и ищет в нем стоп-слова.
-     * @param checkText {@link String} текст со стоп-словами
-     * @return List<String> Список стоп-слов
-     */
-    public List<String> checkComment(String checkText) {
-        String textForCheck = checkText.toLowerCase();
-
-        List<BadWords> allActiveBW = badWordsRepository.getBadWordsActive();
-        List<String> foundWords = new ArrayList<>();
-        for (BadWords wordCheck : allActiveBW) {
-            String findMe = wordCheck.getBadword().toLowerCase();
-            if (textForCheck.matches(".*?\\b" + findMe + "\\b.*?")) {
-                foundWords.add(findMe);
-            }
-        }
-
-        return foundWords;
-    }
-
-
-
-    @Test
-    void preparingWordsForImport() {
-        String incoming="df, gf, dfgdfg,  , fdg,  f, dSF GGH5    sdf  dsfs, sd,.2sd. dsf .sdf f.sd,233.,sdf,  sd,f,sdf,.sdfsd,f,.sdf  df dsfsdfdf,      df,.,,234";
-        String[] actual={"2","3"};
-        String[] expected={"2","3"};
-        assertArrayEquals(expected,actual);
-//        badWordsService.preparingWordsForImport();
-
-        Pattern CLEAR_PATTERN = Pattern.compile("[\\s]+");
-        String noSpace = CLEAR_PATTERN.matcher(incoming).replaceAll(" ").trim();
-//        System.out.println(noSpace+"__________________________");
-        String noCommaEnd = noSpace.replaceAll(",$", "");
-        String[] importText = noCommaEnd.split(", ");
-        for(String a : importText){
-        System.out.println(a);}
-
-    }
-    /**
-     * Метод подготовки стоп-слов для импорта.
-     * Удаляет лишние пробелы, запятые
-     * @param text {@link String} текст для обработки
-     * @return String[] массив стоп-слов
-     */
-    private String[] preparingWordsForImport(String text) {
-        //Очищаем от лишних пробелов
-        Pattern CLEAR_PATTERN = Pattern.compile("[\\s]+");
-        String noSpace = CLEAR_PATTERN.matcher(text).replaceAll(" ").trim();
-
-        //удаляем запятую в конце
-        String noCommaEnd = noSpace.replaceAll(",$", "");
-
-        //Разбиваем на отдельные слова в массив
-        String[] importText = noCommaEnd.split(", ");
-
-        return importText;
-    }
-
-
-//    @Test
-//    void importWord() {
-//    }
-//
-
 }
--- src/test/java/com/jm/online_store/service/impl/BadWordsServiceImplTest3.java
+++ src/test/java/com/jm/online_store/service/impl/BadWordsServiceImplTest3.java
@@ -5,27 +5,25 @@ import com.jm.online_store.repository.BadWordsRepository;
 import com.jm.online_store.service.interf.CommonSettingsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.regex.Pattern;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+//
+//@RunWith(SpringRunner.class)
 
+@SpringBootTest
 public class BadWordsServiceImplTest3 {
     @MockBean
     private BadWordsRepository badWordsRepository;
@@ -35,51 +33,75 @@ public class BadWordsServiceImplTest3 {
     @Autowired
     private BadWordsServiceImpl badWordsService;
 
-
     private List<BadWords> allActiveBW;
-    private List<BadWords> ReturnBadWords;
+    private List<String> expectedBadWords;
     private String incoming;
+    private String incoming2;
+    private BadWords df;
+
+
 
     @BeforeEach
     void init() {
+
+        df = new BadWords("rfrf",true);
+
+    }
+
+
+//
+    @Test
+    void preparingWordsForImport() { }
+
+    public String[] preparingWordsForImport(String text) {
+//    private String[] preparingWordsForImport(String text) {
+        //Очищаем от лишних пробелов
+        Pattern CLEAR_PATTERN = Pattern.compile("[\\s]+");
+        String noSpace = CLEAR_PATTERN.matcher(text).replaceAll(" ").trim();
+
+        //удаляем запятую в конце
+        String noCommaEnd = noSpace.replaceAll(",$", "");
+
+        //Разбиваем на отдельные слова в массив
+        String[] importText = noCommaEnd.split(", ");
+
+        return importText;
+    }
+    @Test
+    void importWord() {
+        incoming2="Winter, , g, Spring, Summer, Autumn, h";
+        incoming="Winter, , g, Spring, Summer, Autumn, h";
+        String[] as= {"Winter", "Spring", "Summer", "Autumn"};
+//        Mockito.when(badWordsService.preparingWordsForImport(incoming)).thenReturn(as);
+//        doReturn(as).when((badWordsService).preparingWordsForImport(incoming));
+//        Mockito.when(badWordsService.findAllWordsActive()).thenReturn(allActiveBW);
+       badWordsService.importWord(incoming2);
+//        verify(badWordsService, times(1)).preparingWordsForImport(any());
+        verify(badWordsRepository, times(4)).existsBadWordsByBadword(any(String.class));
+        verify(badWordsRepository, times(4)).save(any(BadWords.class));
+    }
+
+    /**
+     * Метод принимает текст, отдает на очистку от всего лишнего в preparingWordsForImport
+     * Формат ввода слов в input через запятую. Слово должно быть длинее 1 символа.
+     * @param  {@link String} текст для импорта стоп-слов
+     */
+    @Test
+    void CheckComment() {
+//        when(badWordsService.checkComment(incoming)).thenReturn(expectedBadWords);
+//        doReturn(expectedBadWords).when((badWordsService).checkComment(incoming));
         allActiveBW = Arrays.asList(
                 new BadWords("поршивка",true),
                 new BadWords("евошняя",false),
                 new BadWords("глазница",true),
                 new BadWords("гад",true));
         incoming="lol'Ах ты ПОРШиВка!!!.ббббббр' - говно Закричал поп:'Мне не нужна евошняя Титановая Глазница...GGH5'";
-        ReturnBadWords = Arrays.asList(
-                new BadWords("поршивка",true),
-                new BadWords("евошняя",false),
-                new BadWords("глазница",true));
-    }
 
-    @Test
-    void checkComment() {
-//        when(badWordsRepository.getBadWordsActive()).thenReturn(Optional.ofNullable(allActiveBW));
-//        when(badWordsService.findAllWordsActive()).thenReturn(Optional.ofNullable(allActiveBW));
-
-
-    }
-    /**
-     * Метод принимает текст, и ищет в нем стоп-слова.
-     * @param checkText {@link String} текст со стоп-словами
-     * @return List<String> Список стоп-слов
-     */
-    public List<String> checkComment(String checkText) {
-        String textForCheck = checkText.toLowerCase();
-
-        List<BadWords> allActiveBW = badWordsRepository.getBadWordsActive();
-        List<String> foundWords = new ArrayList<>();
-        for (BadWords wordCheck : allActiveBW) {
-            String findMe = wordCheck.getBadword().toLowerCase();
-            if (textForCheck.matches(".*?\\b" + findMe + "\\b.*?")) {
-                foundWords.add(findMe);
-            }
-        }
-
-        return foundWords;
+        expectedBadWords = Arrays.asList("поршивка", "евошняя", "глазница");
+//        Mockito.when(badWordsRepository.getBadWordsActive()).thenReturn(allActiveBW);
+        Mockito.when(badWordsService.findAllWordsActive()).thenReturn(allActiveBW);
+        List<String> actual = badWordsService.checkComment(incoming);
+        assertEquals(expectedBadWords, actual);
     }
 
 }
-
