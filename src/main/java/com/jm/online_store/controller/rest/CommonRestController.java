b6dab41e65814ef92ba967b04f4d5248400d2e1e
DotsenkoS <6469968@gmail.com>
2021-01-07 17:57:08 +0300

    Added display of store name from application.yml

---
+++ src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
@@ -0,0 +1,32 @@
+package com.jm.online_store.controller.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+@Api(description = "Rest controller for transferring common data")
+public class CommonRestController {
+
+    @Value("${common-data.store-name}")
+    private String storeName;
+
+    /**
+     * контроллер для получения наименвания магизина из application.yml
+     *
+     * @return ResponseEntity<String> наименвание магазина
+     */
+
+    @GetMapping("api/storeName")
+    @ApiOperation(value = "Get store name")
+    public ResponseEntity<String> getStoreName() {
+        return ResponseEntity.ok(storeName);
+    }
+}
