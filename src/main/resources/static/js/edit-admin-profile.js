8025bfe106ab74467582eba673da1daa1120f27b
vyaacheslav-vorobev <vyacheslav-vorobev@example.com>
2021-01-31 06:28:03 +0300

    Добавил возможность изменять название магазина

--- src/main/resources/static/js/edit-admin-profile.js
+++ src/main/resources/static/js/edit-admin-profile.js
@@ -1,5 +1,4 @@
 $("#buttonNameStore").click(async function (){
-    console.log("Yooou")
     let name = document.getElementById("nameStore").value
     await $.ajax("http://localhost:9898/api/editStoreName", {
         method: "put",
