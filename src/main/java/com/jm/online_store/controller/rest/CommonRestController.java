1e82ea99533e8d436436da5bf442dc406f4cbd12
vyaacheslav-vorobev <vyacheslav-vorobev@example.com>
2021-02-02 05:13:53 +0300

    normalСommit

--- src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
@@ -1,11 +1,15 @@
 package com.jm.online_store.controller.rest;
 
+import com.jm.online_store.model.CommonSettings;
+import com.jm.online_store.service.interf.CommonSettingsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,17 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(description = "Rest controller for transferring common data")
 public class CommonRestController {
 
-    @Value("${common-data.store-name}")
-    private String storeName;
+    private CommonSettingsService commonSettingsService;
+
+    @Autowired
+    public CommonRestController(CommonSettingsService commonSettingsService){
+        this.commonSettingsService = commonSettingsService;
+    }
 
     /**
-     * Контроллер для получения наименования магазина из application.yml
-     * @return ResponseEntity<String> наименование магазина
+     * Контроллер для получения наименования магазина из БД
      */
-
     @GetMapping("/storeName")
     @ApiOperation(value = "Get store name")
-    public ResponseEntity<String> getStoreName() {
-        return ResponseEntity.ok(storeName);
+    public String getStoreName() {
+        return  commonSettingsService.getSettingByName("store_name").getTextValue();
+    }
+
+    /**
+     * Контроллер для изменение наименования магазина
+     */
+    @PutMapping("/editStoreName")
+    @ApiOperation(value = "edit store name")
+    public void editStoreName(CommonSettings commonSettings){
+        commonSettingsService.updateTextValue(commonSettings);
     }
 }
--- src/main/resources/static/js/profile-admin-manager-service.js
+++ src/main/resources/static/js/profile-admin-manager-service.js
@@ -55,6 +55,43 @@ function changePassword() {
         }
     })
 }
+/**
+ * Функция для смены названия магазина
+ */
+$("#buttonNameStore").click(function (){
+    let name = document.getElementById("nameStore").value
+    $.ajax("http://localhost:9898/api/editStoreName", {
+        method: "put",
+        data: {
+            settingName:"store_name",
+            textValue: name,
+            status: false,
+        },
+        dataType: "text",
+        success: function () {
+            $('#changeStoreName').modal('hide')
+        }
+    })
+})
+
+/**
+ * Функция для смены названия магазина
+ */
+$("#buttonNameStore").click(function (){
+    let name = document.getElementById("nameStore").value
+    $.ajax("http://localhost:9898/api/editStoreName", {
+        method: "put",
+        data: {
+            settingName:"store_name",
+            textValue: name,
+            status: false,
+        },
+        dataType: "text",
+        success: function () {
+            $('#changeStoreName').modal('hide')
+        }
+    })
+})
 
 /**
  * Функция получения текущего юзера и заполнение полей профиля
