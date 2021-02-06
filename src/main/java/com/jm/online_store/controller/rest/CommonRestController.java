0b1b6f7d87f9a3854febbbef8bced22d447d64c5
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-02-06 20:08:33 +0300

    remove unusable import

--- src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
@@ -2,7 +2,6 @@ package com.jm.online_store.controller.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
