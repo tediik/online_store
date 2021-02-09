c8f48ce917d6bfdca61dc04d7b5f28152284e6e7
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-02-09 17:40:43 +0300

    create exception for customerService

---
+++ src/main/java/com/jm/online_store/exception/customer/CustomerServiceException.java
@@ -0,0 +1,7 @@
+package com.jm.online_store.exception.customer;
+
+public class CustomerServiceException extends RuntimeException{
+    public CustomerServiceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/jm/online_store/exception/customer/ExceptionCustomerConstants.java
@@ -0,0 +1,7 @@
+package com.jm.online_store.exception.customer;
+
+public class ExceptionCustomerConstants {
+    public final static String CUSTOMER_ARE_NOT_AUTHENTICATED = "YOU ARE NOT AUTHENTICATED, PLEASE LOGG IN";
+    public final static String CUSTOMER_EMAIL_ALREADY_EXISTS = "THIS EMAIL ALREADY EXISTS";
+    public final static String CUSTOMER_EMAIL_NOT_VALID = "YOUR EMAIL ADDRESS IS NOT VALID, PLEASE TRY AGAIN";
+}
