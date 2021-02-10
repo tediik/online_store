b5fdcfc3798036f76b8503a93b79a79f68838e64
vyaacheslav-vorobev <vyacheslav-vorobev@example.com>
2021-02-11 02:57:47 +0300

    CommonRestController and AdminRestController

--- src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/CommonRestController.java
@@ -1,13 +1,11 @@
 package com.jm.online_store.controller.rest;
 
-import com.jm.online_store.model.CommonSettings;
 import com.jm.online_store.service.interf.CommonSettingsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,22 +18,12 @@ public class CommonRestController {
     private final CommonSettingsService commonSettingsService;
 
     /**
-     * Контроллер для получения наименования магазина из БД
+     * Метод для получения наименования магазина из БД
      * @return ResponseEntity<String> наименование магазина
      */
     @GetMapping("/storeName")
     @ApiOperation(value = "Get store name")
     public ResponseEntity<String> getStoreName() {
         return ResponseEntity.ok(commonSettingsService.getSettingByName("store_name").getTextValue());
-    }
-
-    /**
-     * Контроллер для изменения наименования магазина
-     * @param commonSettings настройки, содержащие название магазина
-     */
-    @PutMapping("/admin/editStoreName")
-    @ApiOperation(value = "edit store name")
-    public void editStoreName(CommonSettings commonSettings){
-        commonSettingsService.updateTextValue(commonSettings);
     }
 }
