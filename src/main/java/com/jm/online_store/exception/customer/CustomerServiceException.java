c4c635638c155c6d7085fb603383d274decc24ef
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-02-11 00:24:14 +0300

    fix CustomerServiceImpl

--- src/main/java/com/jm/online_store/exception/customer/CustomerServiceException.java
+++ src/main/java/com/jm/online_store/exception/customer/CustomerServiceException.java
@@ -1,18 +1,12 @@
 package com.jm.online_store.exception.customer;
 
-import org.springframework.http.HttpStatus;
 
 public class CustomerServiceException extends RuntimeException {
-    private String message;
-    private HttpStatus httpStatus;
 
 
     public CustomerServiceException(String message) {
         super(message);
     }
 
-    public CustomerServiceException(String message , HttpStatus httpStatus) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-    }
+
 }
