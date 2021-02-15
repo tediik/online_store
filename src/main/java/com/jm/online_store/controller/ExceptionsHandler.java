c36cfaceed77247ab51307cfdbd62e47f0e2c48a
Eduard Nabiev <32924575+tediik@users.noreply.github.com>
2021-02-15 15:23:09 +0300

    Update ExceptionsHandler.java

--- src/main/java/com/jm/online_store/controller/ExceptionsHandler.java
+++ src/main/java/com/jm/online_store/controller/ExceptionsHandler.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 public class ExceptionsHandler {
 
