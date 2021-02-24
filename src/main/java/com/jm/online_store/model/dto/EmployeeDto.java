b14ba1e053eee93c35c8f0f3bdcc07ad591b1430
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-02-24 03:45:12 +0300

    create EmployeeDto and EmployeeService

---
+++ src/main/java/com/jm/online_store/model/dto/EmployeeDto.java
@@ -0,0 +1,4 @@
+package com.jm.online_store.model.dto;
+
+public class EmployeeDto {
+}
---
+++ src/main/java/com/jm/online_store/service/interf/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.jm.online_store.service.interf;
+
+import com.jm.online_store.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAllEmployee();
+
+}
