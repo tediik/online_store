d8afe2f144316e19f17ce8ffb1beb1f16fd50747
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-03-01 00:04:44 +0300

    check all except service layer

--- src/main/java/com/jm/online_store/service/interf/EmployeeService.java
+++ src/main/java/com/jm/online_store/service/interf/EmployeeService.java
@@ -1,9 +1,6 @@
 package com.jm.online_store.service.interf;
 
 import com.jm.online_store.model.Employee;
-import com.jm.online_store.model.dto.EmployeeDto;
-
-import java.util.List;
 
 public interface EmployeeService {
 
