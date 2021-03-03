dff790bb43c91c85e82da2cffd9d5bed9ecc8759
Buravtseva Ekaterina <buravtseva96@mail.ru>
2021-03-03 16:35:56 +0300

    Cosmetic changes in ProductController

--- src/main/java/com/jm/online_store/controller/simple/ProductController.java
+++ src/main/java/com/jm/online_store/controller/simple/ProductController.java
@@ -1,11 +1,9 @@
 package com.jm.online_store.controller.simple;
 
 import com.jm.online_store.exception.ProductNotFoundException;
-import com.jm.online_store.model.Product;
 import com.jm.online_store.service.interf.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
