eb6b620279046b2f9ad7c82c28d3bbbae3b08d50
Ponin Fedor <fponin@gmail.com>
2021-03-05 15:45:29 +0300

    refactor/add MaxUploadSizeException

--- src/main/java/com/jm/online_store/controller/rest/ImageRestController.java
+++ src/main/java/com/jm/online_store/controller/rest/ImageRestController.java
@@ -1,17 +1,21 @@
 package com.jm.online_store.controller.rest;
 
+import com.jm.online_store.enums.ResponseOperation;
 import com.jm.online_store.model.User;
+import com.jm.online_store.model.dto.ResponseDto;
 import com.jm.online_store.service.interf.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -27,9 +31,16 @@ public class ImageRestController {
     @PostMapping("/upload")
     @ApiOperation(value = "uploads images",
             authorizations = { @Authorization(value = "jwtToken") })
-    public ResponseEntity<String> handleImagePost(@RequestParam("imageFile") MultipartFile imageFile) throws IOException {
+    public ResponseEntity<ResponseDto<String>> handleImagePost(@RequestParam("imageFile") MultipartFile imageFile) throws IOException {
         User userDetails = userService.getCurrentLoggedInUser();
-        return ResponseEntity.ok(userService.updateUserImage(userDetails.getId(), imageFile));
+        try {
+            userService.updateUserImage(userDetails.getId(), imageFile);
+            return new ResponseEntity<>(
+                    new ResponseDto<>(true, "Image was upload", ResponseOperation.NO_ERROR.getMessage()),
+                    HttpStatus.OK);
+        } catch (MaxUploadSizeExceededException | IOException ex){
+            throw ex;
+        }
     }
 
     @DeleteMapping("/delete")
