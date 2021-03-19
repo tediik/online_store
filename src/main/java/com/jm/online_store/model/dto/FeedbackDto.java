0ef335a502947bbb69799ba72921a67900aba7de
Dalan2692 <30412996+Dalan2692@users.noreply.github.com>
2021-03-19 17:09:49 +0300

    bug fix

--- src/main/java/com/jm/online_store/model/dto/FeedbackDto.java
+++ src/main/java/com/jm/online_store/model/dto/FeedbackDto.java
@@ -1,46 +1,19 @@
 package com.jm.online_store.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.jm.online_store.model.Feedback;
-import com.jm.online_store.model.Topic;
-import com.jm.online_store.model.User;
 import io.swagger.annotations.ApiModel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 @Data
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
-@ApiModel(description =  "Dto для Feedback - обратная связь, связана с User")
+@Slf4j
+@ApiModel(description = "DTO для сущности комментария FeedBack")
+@Component
 public class FeedbackDto {
-
     private Long id;
-
-    private Topic topic;
-
-    private User user;
-
     private Feedback.Status status;
-
     private String message;
-
     private String answer;
-
     private Long managerId;
-
-    private LocalDateTime responseExpected;
-
-    private LocalDateTime feedbackPostDate;
-
-    public enum Status {
-        IN_PROGRESS,
-        LATER,
-        RESOLVED
-    }
 }
